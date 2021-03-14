26e489ca3db9270ed9fc17fa0453ddbe9653c4a8
Rastislav Domaniský <63097727+rastislav-domanisky@users.noreply.github.com>
2021-03-14 14:22:22 +0100

    updated ListView component

--- src/views/ListView/ListView.tsx
+++ src/views/ListView/ListView.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import './ListView.scss';
+
+import SearchInput from '../../components/SearchInput/SearchInput';
+import IconButton from '../../components/IconButton/IconButton';
+
+import AddIcon from '../../assets/add_icon.svg';
+
+const addUserHandle = () => {
+    console.log("Add User")
+}
 
 const ListView = () => {
     return (
         <div className="List-View">
-            LET.IS
+            <header>
+                <div className="row">
+                    <SearchInput />
+                    <IconButton icon={AddIcon} onPress={addUserHandle} />
+                </div>
+            </header>
+            <main>
+
+            </main>
         </div>
     );
 }
