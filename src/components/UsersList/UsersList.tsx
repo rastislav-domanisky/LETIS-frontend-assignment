155c986890b3648e7790650e2b0aa86cecceb85d
Rastislav Domaniský <63097727+rastislav-domanisky@users.noreply.github.com>
2021-03-14 15:00:44 +0100

    created UsersList component

---
+++ src/components/UsersList/UsersList.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './UsersList.scss';
+
+const UsersList: React.FC = () => {
+    return (
+        <div className="Users-List">
+            
+        </div>
+    );
+}
+
+export default UsersList;
+
+
