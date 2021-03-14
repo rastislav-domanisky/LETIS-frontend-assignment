4850d022efb86a133dd2da9d739f916d693588e8
Rastislav Domaniský <63097727+rastislav-domanisky@users.noreply.github.com>
2021-03-14 13:28:41 +0100

    installed Router and added ListView

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import './App.css';
+
+import ListView from './views/ListView/ListView';
 
 function App() {
   return (
     <div className="App">
-      LET.IS
+      <Router>
+        <Switch>
+          <Route path="/" component={ListView} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/views/ListView/ListView.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ListView = () => {
+    return (
+        <div className="List-View">
+            LET.IS
+        </div>
+    );
+}
+
+export default ListView;
+
