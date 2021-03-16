0c62ff5dabf5ded92708f20a13e9f14482e041d1
Rastislav Domaniský <63097727+rastislav-domanisky@users.noreply.github.com>
2021-03-16 09:07:01 +0100

    added script.py

---
+++ script.py
@@ -0,0 +1,13 @@
+import json
+
+data = ""
+
+with open('response.json', encoding='utf-8') as json_file:
+          data = json.load(json_file)
+          index = 0;
+          for usr in data:
+              usr['id'] = index
+              index = index+1
+                     
+with open('data.json', 'w',) as outfile:
+    json.dump(data, outfile)
